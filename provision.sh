b88b7702ed10f18a25c77d63c3813e321f79c5f2
Ubuntu <ubuntu@ip-172-31-9-199.us-west-2.compute.internal>
2017-04-04 15:15:50 +0000

    devEnvironment running provision.sh

---
+++ provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update the souces list
+sudo apt-get update -y
+
+# Upgrade any packages available
+sudo apt-get upgrade -y
+
+# Install the package for nginx
+sudo apt-get install nginx -y
+
+# install node.js  version 7
+sudo npm cache clean -f
+sudo apt-get install -y nodejs
+
+# install node.js  version 7
+# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+
+# install the pm2 from npm
+npm install -g pm2
+
+#  Install the git 
+apt-get install git -y
+
+
+
+# npm install
+# node app.js
